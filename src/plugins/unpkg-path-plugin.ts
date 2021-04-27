ae250803f37808abe050f35e7af316d45e2c0bfa
DESKTOP-3G40DR1\Diva <diva.lonial@gmail.com>
2021-04-27 21:12:25 +0800

    Update esbuild plugin to handle nested imports

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,26 +1,53 @@
-import * as esbuild from 'esbuild-wasm';
- 
+import axios from "axios";
+import * as esbuild from "esbuild-wasm";
+
 export const unpkgPathPlugin = () => {
   return {
-    name: 'unpkg-path-plugin',
+    name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        return { path: args.path, namespace: 'a' };
-      });
- 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        console.log('onLoad', args);
- 
-        if (args.path === 'index.js') {
+        console.log("onResolve", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        }
+
+        if (args.path.includes("./") || args.path.includes("../")) {
           return {
-            loader: 'jsx',
+            namespace: "a",
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
+          };
+        }
+
+        return {
+          namespace: "a",
+          path: `https://unpkg.com/${args.path}`,
+        };
+      });
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log("onLoad", args);
+
+        if (args.path === "index.js") {
+          return {
+            loader: "jsx",
             contents: `
-              import message from 'tiny-test-pkg';
+              const message = require('react');
               console.log(message);
             `,
           };
-        } 
+        }
+
+        const { data, request } = await axios.get(args.path);
+        console.log(data);
+
+        return {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
       });
     },
   };
