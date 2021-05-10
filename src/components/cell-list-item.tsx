bc55cb285ecc3e12e934f13a28aeb44a1a691ae3
Diva Lonial <diva.lonial@gmail.com>
2021-05-10 21:55:36 +0800

    Add in action bar (move cells up / down / delete) above cells

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,4 +1,6 @@
 import { Cell } from "../state";
+import './cell-list-item.css'
+import ActionBar from "./action-bar";
 import CodeCell from "./code-cell";
 import TextEditor from "./text-editor";
 
@@ -10,13 +12,23 @@ const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
     let child: JSX.Element;
 
     if (cell.type === 'code') {
-        child = <CodeCell cell={cell} />
+        child = <>
+            <div className="action-bar-wrapper" >
+                <ActionBar id={cell.id}></ActionBar>
+            </div>
+            <CodeCell cell={cell} />
+        </>
     } else {
-        child = <TextEditor cell={cell} />
+        // react fragment groups these together without rendering in div?
+        child = <>
+            <TextEditor cell={cell} />
+            {/* displaying action bar after renders it on top */}
+            <ActionBar id={cell.id}></ActionBar>
+        </>
     }
 
     return (
-        <div >
+        <div className="cell-list-item">
             {child}
         </div>
     );
